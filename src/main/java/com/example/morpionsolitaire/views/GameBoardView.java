69a1d540795883cd2ad9de609ce8ad0c86401398
adchejjari <a.chejjari@gmail.com>
2022-11-03 23:20:41 +0100

    draw init cross on game board

--- src/main/java/com/example/morpionsolitaire/views/GameBoardView.java
+++ src/main/java/com/example/morpionsolitaire/views/GameBoardView.java
@@ -9,12 +9,32 @@ import javafx.scene.shape.Circle;
 
 public class GameBoardView {
 
-    final static int BOARD_WIDTH = 17;
+    final static int BOARD_WIDTH = 18;
     final static int BOARD_HEIGHT = 18;
 
     @FXML
     public TilePane grid;
     public Group group;
+
+    int[][] GameMatrix = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //1
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //2
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //3
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //4
+            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 }, //5
+            { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, //6
+            { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, //7
+            { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0 }, //8
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, //9
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, //10
+            { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0 }, //11
+            { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, //12
+            { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, //13
+            { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0 }, //14
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //15
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 16
+    };
 
     @FXML
     void initialize() {
@@ -24,9 +44,13 @@ public class GameBoardView {
                 group.getChildren().add(b);
             }
         }
-        Circle circle = new Circle(350, 350, Dot.RADIUS, Paint.valueOf("green"));
-        Circle circle2 = new Circle(105, 105, Dot.RADIUS, Paint.valueOf("green"));
-        group.getChildren().add(circle);
-        group.getChildren().add(circle2);
+        for (int i = 0; i<16;i++){
+            for(int j = 0;j<16;j++){
+                if (this.GameMatrix[i][j]==1){
+                    Circle circle = new Circle(j * 35, i*35, Dot.RADIUS, Paint.valueOf("green"));
+                    group.getChildren().add(circle);
+                }
+            }
+        }
     }
 }
