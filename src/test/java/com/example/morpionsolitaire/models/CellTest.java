3d38f1dac868f576a301ba59887622c40b4d4a12
mathuschan <mathu93420@gmail.com>
2023-01-04 21:16:05 +0100

    Test unitaire

--- src/test/java/com/example/morpionsolitaire/models/CellTest.java
+++ src/test/java/com/example/morpionsolitaire/models/CellTest.java
@@ -2,18 +2,146 @@ package com.example.morpionsolitaire.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CellTest {
 
+    
     @Test
-    void getLinkedNodes() {
-        assertEquals(null,Cell.getLinkedNodes());
+    public void testSetLink() {
+        // Create a cell and a link
+        Cell cell = new Cell(0, 0, 0);
+        Link link = new Link(cell, new Cell[] { cell }, LinkType.HORIZONTAL);
+
+        // Set the link for the cell
+        cell.setLink(link);
+
+        // Check that the cell's linked nodes are correct
+        assertEquals(link.getNodes(), cell.getLinkedNodes().getNodes());
+
+        // Check that the link's cells are correct
+        assertTrue(Arrays.asList(link.getNodes()).contains(cell));
+    }
+    @Test
+    public void testUndoLink() {
+        // Create a cell and a link
+        Cell cell1 = new Cell(0, 0, 0);
+        Cell cell2 = new Cell(1, 1, 1);
+        Link link = new Link(cell1, new Cell[] { cell1, cell2 }, LinkType.HORIZONTAL);
+        cell1.setLink(link);
+
+        // Set the main link type for the cells
+        LinkType mainLink = LinkType.HORIZONTAL;
+        cell1.setMainLink(mainLink);
+        cell2.setMainLink(mainLink);
+
+        // Link the cells with the main link type
+        cell1.link(mainLink);
+        cell2.link(mainLink);
+
+        // Check that the cells are linked with the main link type
+        assertTrue(cell1.isLinked(mainLink));
+        assertTrue(cell2.isLinked(mainLink));
+
+        // Undo the link
+        cell1.undoLink();
+
+        // Check that the cells are no longer linked with the main link type
+        assertFalse(cell1.isLinked(mainLink));
+        assertFalse(cell2.isLinked(mainLink));
     }
 
     @Test
-    void hasMainLink() {
-        assertEquals(false,Cell.hasMainLink());
+    public void testLink() {
+        // Create a cell
+        Cell cell = new Cell(0, 0, 0);
+
+        // Link the cell with a horizontal link
+        LinkType linkType = LinkType.HORIZONTAL;
+        cell.link(linkType);
+
+        // Check that the cell is linked with a horizontal link
+        assertTrue(cell.isLinked(linkType));
+    }
+
+    @Test
+    public void testSetValue() {
+        // Create a cell
+        Cell cell = new Cell(0, 0, 0);
+
+        // Set the value of the cell
+        int value = 1;
+        cell.setValue(value);
+
+        // Check that the cell's value is correct
+        assertEquals(value, cell.getValue());
+    }
+
+    @Test
+    public void testIsLinked() {
+        // Create a cell
+        Cell cell = new Cell(0, 0, 0);
+
+        // Link the cell with a horizontal link
+        LinkType linkType = LinkType.HORIZONTAL;
+        cell.link(linkType);
+
+        // Check that the cell is linked with a horizontal link
+        assertTrue(cell.isLinked(linkType));
+
+        // Check that the cell is not linked with a vertical link
+        assertFalse(cell.isLinked(LinkType.VERTICAL));
+    }
+
+    @Test
+    public void testSetMainLink() {
+        // Create a cell
+        Cell cell = new Cell(0, 0, 0);
+
+        // Set the main link type for the cell
+        LinkType mainLink = LinkType.HORIZONTAL;
+        cell.setMainLink(mainLink);
+
+        // Check that the main link type for the cell is correct
+        assertEquals(mainLink, cell.getMainLink());
+    }
+
+    @Test
+    public void testHasMainLink() {
+        // Create a cell with a main link
+        Cell cell = new Cell(0, 0, 0);
+        LinkType mainLink = LinkType.HORIZONTAL;
+        cell.setMainLink(mainLink);
+
+        // Check that the cell has a main link
+        assertTrue(cell.hasMainLink());
+
+        // Create a cell with no main link
+        Cell cell2 = new Cell(1, 1, 1);
+
+        // Check that the cell has no main link
+        assertTrue(cell2.hasMainLink());
+    }
+
+    @Test
+    public void testUnlink() {
+        // Create a cell
+        Cell cell = new Cell(0, 0, 0);
+
+        // Link the cell with a horizontal link
+        LinkType linkType = LinkType.HORIZONTAL;
+        cell.link(linkType);
+
+        // Check that the cell is linked with a horizontal link
+        assertTrue(cell.isLinked(linkType));
+
+        // Unlink the cell
+        cell.unlink(linkType);
+
+        // Check that the cell is not linked with a horizontal link
+        assertFalse(cell.isLinked(linkType));
     }
 
 }
--- src/test/java/com/example/morpionsolitaire/models/GridTest.java
+++ src/test/java/com/example/morpionsolitaire/models/GridTest.java
@@ -1,61 +1,65 @@
 package com.example.morpionsolitaire.models;
 
+import javafx.beans.value.ObservableBooleanValue;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static javafx.beans.binding.Bindings.when;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GridTest {
 
+
     @Test
-    void load() {
+    public void testSetCell() throws IOException {
+        // Create a new Grid object
+        Grid grid = new Grid();
+
+        // Set the value of a cell in the grid
+        grid.setCell(0, 0, 1);
+
+        // Verify that the value of the cell was set correctly
+        assertEquals(1, grid.getCell(0, 0).getValue());
     }
 
     @Test
-    void setCell() {
+    public void testGetCell() throws IOException {
+        // Create a new Grid object
+        Grid grid = new Grid();
+
+        // Set the value of the Cell at position (0, 0) to 1 using the setCell method
+        grid.setCell(0, 0, 1);
+
+        // Get the Cell object at position (0, 0) using the getCell method
+        Cell cell = grid.getCell(0, 0);
+
+        // Check if the value of the Cell object is 1
+        assertEquals(1, cell.getValue());
     }
 
     @Test
-    void getCell() {
-        assertEquals(null,Grid.getCell(7,8));
+    public void testIsCellEmpty() throws IOException {
+        // Create a new Grid object
+        Grid grid = new Grid();
+
+        // Set the value of the Cell at position (0, 0) to 0 using the setCell method
+        grid.setCell(0, 0, 0);
+
+        // Check if the isCellEmpty method returns true for the Cell at position (0, 0)
+        assertTrue(grid.isCellEmpty(0, 0));
+
+        // Set the value of the Cell at position (0, 0) to 1 using the setCell method
+        grid.setCell(0, 0, 1);
+
+        // Check if the isCellEmpty method returns false for the Cell at position (0, 0)
+        assertFalse(grid.isCellEmpty(0, 0));
     }
 
-    @Test
-    void getMatrix() {
-    }
-
-    @Test
-    void isCellEmpty() {
-    }
-
-    @Test
-    void canJoinVertically() {
-    }
-
-    @Test
-    void canJoinHorizontally() {
-    }
-
-    @Test
-    void getSecondDiagonalInitPosition() {
-    }
-
-    @Test
-    void getFirstDiagonalInitPosition() {
-    }
-
-    @Test
-    void canJoinSecondDiagonal() {
-    }
-
-    @Test
-    void canJoinFirstDiagonal() {
-    }
-
-    @Test
-    void canLink() {
-    }
-
-    @Test
-    void resetCell() {
-    }
 }
