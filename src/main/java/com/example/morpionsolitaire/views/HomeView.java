abd903dfb47c970d32c007dd1a4fd6977abd11a7
adchejjari <a.chejjari@gmail.com>
2023-01-05 23:37:01 +0100

    add main image

--- src/main/java/com/example/morpionsolitaire/views/HomeView.java
+++ src/main/java/com/example/morpionsolitaire/views/HomeView.java
@@ -1,8 +1,14 @@
 package com.example.morpionsolitaire.views;
 
+import javafx.scene.image.Image;
 import javafx.scene.layout.TilePane;
 
 public class HomeView {
 
     public TilePane container;
+    public Image mainImage;
+
+    public HomeView(){
+
+    }
 }
